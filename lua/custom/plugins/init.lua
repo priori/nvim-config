a6b84d0d6a427ba369eec5e56a6523ad73ee2212
priori <leonardopriori@gmail.com>
2025-04-13 08:35:23 -0300

    Add hop plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,38 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'smoka7/hop.nvim',
+  version = '*',
+  opts = {
+    keys = 'etovxqpdygfblzhckisuran',
+  },
+  config = function()
+    local hop = require 'hop'
+    local hint = require 'hop.hint'
+    vim.keymap.set('', '<leader>1', function()
+      hop.hint_char1 { current_line_only = false }
+    end, { remap = true })
+    vim.keymap.set('', '<leader>2', function()
+      hop.hint_char2 { current_line_only = false }
+    end, { remap = true })
+    vim.keymap.set('', '<leader>f', function()
+      hop.hint_words { current_line_only = false }
+    end, { remap = true })
+
+    vim.keymap.set('', 'f', function()
+      hop.hint_char1 { direction = hint.HintDirection.AFTER_CURSOR, current_line_only = true }
+    end, { remap = true })
+    vim.keymap.set('', 'F', function()
+      hop.hint_char1 { direction = hint.HintDirection.BEFORE_CURSOR, current_line_only = true }
+    end, { remap = true })
+    vim.keymap.set('', 't', function()
+      hop.hint_char1 { direction = hint.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }
+    end, { remap = true })
+    vim.keymap.set('', 'T', function()
+      hop.hint_char1 { direction = hint.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }
+    end, { remap = true })
+
+    hop.setup()
+  end,
+}
