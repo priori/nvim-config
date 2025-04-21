f9e17189e72400372363d569e18654c2696cfcaa
priori <leonardopriori@gmail.com>
2025-04-20 23:14:50 -0300

    Add nvim-ts-autotag plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -36,4 +36,12 @@ return {
 
     hop.setup()
   end,
+  {
+    'windwp/nvim-ts-autotag',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
 }
