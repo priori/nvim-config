76c11202268e43094bb318322f188ca5c958b251
priori <leonardopriori@gmail.com>
2025-04-23 21:55:42 -0300

    Add nvim-treesitter-context plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -65,4 +65,19 @@ return {
       require('nvim-ts-autotag').setup()
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'VeryLazy',
+    config = function()
+      require('treesitter-context').setup {
+        enable = true,
+        max_lines = 0,
+        trim_scope = 'outer',
+        min_window_height = 0,
+        line_numbers = true,
+        mode = 'cursor',
+        separator = nil,
+      }
+    end,
+  },
 }
