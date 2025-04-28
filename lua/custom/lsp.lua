eb3881a45a4efd896da1183cfd2416e6e801d092
priori <leonardopriori@gmail.com>
2025-04-27 22:22:27 -0300

    Update <enter> got to definitions

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -6,6 +6,20 @@ return function(event)
     vim.keymap.set('n', keys, func, { buffer = event.buf, desc = desc })
   end
 
+  local function find_file_win(filename)
+    local windows = vim.api.nvim_list_wins()
+    for _, win in ipairs(windows) do
+      local buf = vim.api.nvim_win_get_buf(win)
+      if vim.api.nvim_buf_is_loaded(buf) then
+        local name = vim.api.nvim_buf_get_name(buf)
+        if name == filename then
+          return win
+        end
+      end
+    end
+    return nil
+  end
+
   nmap('<enter>', function()
     local cur_win = vim.api.nvim_get_current_win()
     local cur_buf = vim.api.nvim_get_current_buf()
@@ -33,20 +47,39 @@ return function(event)
         if win_after_l ~= cur_win then
           target_win = win_after_l
         else
-          -- Se não mudou, tenta a janela à esquerda
           vim.cmd 'wincmd h'
           local win_after_h = vim.api.nvim_get_current_win()
+          vim.cmd 'wincmd h'
+          local win_after_h2 = vim.api.nvim_get_current_win()
           if win_after_h ~= cur_win then
-            target_win = win_after_h
+            if win_after_h2 == win_after_h then
+              target_win = nil
+            else
+              target_win = win_after_h
+            end
           end
         end
-        if target_win then
-          vim.api.nvim_win_set_buf(target_win, cur_buf)
-          vim.api.nvim_win_set_cursor(target_win, cursor)
+        local file_win = find_file_win(filename)
+        if not target_win then
+          if file_win then
+            target_win = file_win
+          else
+            vim.cmd 'vsplit'
+            vim.cmd 'wincmd l'
+            target_win = vim.api.nvim_get_current_win()
+          end
         else
-          vim.cmd 'vsplit'
-          target_win = vim.api.nvim_get_current_win()
+          local target_buf = vim.api.nvim_win_get_buf(target_win)
+          local target_file_name = vim.api.nvim_buf_get_name(target_buf)
+          if file_win and file_win ~= target_win and target_file_name ~= filename then
+            target_win = file_win
+          end
         end
+        local result_size = table.getn(result)
+        vim.api.nvim_set_current_win(target_win)
+
+        vim.api.nvim_win_set_buf(target_win, cur_buf)
+        vim.api.nvim_win_set_cursor(target_win, cursor)
         require('telescope.builtin').lsp_definitions {
           show_line = false,
         }
