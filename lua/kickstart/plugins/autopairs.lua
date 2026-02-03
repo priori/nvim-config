f65fc36aee1a735f91381ff7b9f20017671c918d
priori <leonardopriori@gmail.com>
2026-02-03 09:34:16 -0300

    Disable autoparis (", {, [, etc)

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -5,4 +5,18 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
+  config = function(_, opts)
+    local npairs = require 'nvim-autopairs'
+
+    -- Run default setup first
+    npairs.setup(opts)
+
+    -- Remove pairs the user doesn't want to auto-complete
+    npairs.remove_rule('"')
+    npairs.remove_rule('{')
+    npairs.remove_rule('[')
+    npairs.remove_rule('(')
+    npairs.remove_rule('`')
+    npairs.remove_rule("'")
+  end,
 }
